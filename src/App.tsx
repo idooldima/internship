43dc950681cd32b0ec13433e9854be7875f256bd
idooldima <idooldima@gmail.com>
2022-06-03 13:49:06 +0300

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
-import Home from './pages/home';
+import Home from './pages/home/home.component';
 import SignIn from './pages/home/auth/signIn/signIn.component';
 import SignUp from './pages/home/auth/signUp/signUp.component';
 import Header from './pages/home/header/header.component';
